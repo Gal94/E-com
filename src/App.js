c77f4d7f0a1c95814a7e5fb28937d23cb398dac3
Gal94 <yacobi.gal94@gmail.com>
2019-09-21 20:53:39 +0300

    Added links to the directories

--- src/App.js
+++ src/App.js
@@ -3,29 +3,11 @@ import './App.css';
 import { Switch, Route, Link } from 'react-router-dom';
 import HomePage from "./pages/homepage/homepage.component";
 
-export const TopicDetail = () => (
-    <div>
-        <h1>HATS4U</h1>
-    </div>
-);
-
-//link and button are equivalent
-const TopicsList = (props) => (
-    <div>
-        <Link to ='/' > Home</Link>
-        <button onClick={()=> props.history.push('/')}>Topics</button>
-        <h1>HATS</h1>
-    </div>
-);
-
-
 function App() {
   return (
     <div className="App">
         <Switch>
              <Route exact path='/' component={HomePage}/>
-             <Route exact path='/topics' component={TopicsList}/>
-             <Route path='/topics/:topicId' component={TopicDetail}/>
         </Switch>
     </div>
   );
